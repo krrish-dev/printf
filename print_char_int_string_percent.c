7230bd1e0a69e57faca1351c4659216e7971f854
krrish_dev <justbegintoday@gmail.com>
2023-10-20 04:32:57 +0300

    test

--- print_char_int_string_percent.c
+++ print_char_int_string_percent.c
@@ -9,16 +9,19 @@
  */
 int print_char(va_list ap, params_t *params)
 {
-	char pad_char = ' ';
-	unsigned int pad = 1, sum = 0, ch = va_arg(ap, int);
+	char padding_char = ' ';
+	unsigned int padding = 1, count = 0, character = va_arg(ap, int);
 
 	if (params->minus_flag)
-		sum += _putchar(ch);
-	while (pad++ < params->width)
-		sum += _putchar(pad_char);
+		count += _putchar(character);
+
+	while (padding++ < params->width)
+		count += _putchar(padding_char);
+
 	if (!params->minus_flag)
-		sum += _putchar(ch);
-	return (sum);
+		count += _putchar(character);
+
+	return count;
 }
 
 /**
@@ -30,15 +33,16 @@ int print_char(va_list ap, params_t *params)
  */
 int print_int(va_list ap, params_t *params)
 {
-	long l;
+	long integer;
 
 	if (params->l_modifier)
-		l = va_arg(ap, long);
+		integer = va_arg(ap, long);
 	else if (params->h_modifier)
-		l = (short int)va_arg(ap, int);
+		integer = (short int)va_arg(ap, int);
 	else
-		l = (int)va_arg(ap, int);
-	return (print_number(convert(l, 10, 0, params), params));
+		integer = (int)va_arg(ap, int);
+
+	return print_number(convert(integer, 10, 0, params), params);
 }
 
 /**
@@ -50,37 +54,42 @@ int print_int(va_list ap, params_t *params)
  */
 int print_string(va_list ap, params_t *params)
 {
-	char *str = va_arg(ap, char *), pad_char = ' ';
-	unsigned int pad = 0, sum = 0, i = 0, j;
+	char *text = va_arg(ap, char *), padding_char = ' ';
+	unsigned int padding = 0, count = 0, i = 0, j;
 
 	(void)params;
-	switch ((int)(!str))
-		case 1:
-			str = NULL_STRING;
 
-	j = pad = _strlen(str);
-	if (params->precision < pad)
-		j = pad = params->precision;
+	switch ((int)(!text))
+		case 1:
+			text = NULL_STRING;
+
+	j = padding = _strlen(text);
+
+	if (params->precision < padding)
+		j = padding = params->precision;
 
 	if (params->minus_flag)
 	{
 		if (params->precision != UINT_MAX)
-			for (i = 0; i < pad; i++)
-				sum += _putchar(*str++);
+			for (i = 0; i < padding; i++)
+				count += _putchar(*text++);
 		else
-			sum += _puts(str);
+			count += _puts(text);
 	}
+
 	while (j++ < params->width)
-		sum += _putchar(pad_char);
+		count += _putchar(padding_char);
+
 	if (!params->minus_flag)
 	{
 		if (params->precision != UINT_MAX)
-			for (i = 0; i < pad; i++)
-				sum += _putchar(*str++);
+			for (i = 0; i < padding; i++)
+				count += _putchar(*text++);
 		else
-			sum += _puts(str);
+			count += _puts(text);
 	}
-	return (sum);
+
+	return count;
 }
 
 /**
@@ -94,7 +103,7 @@ int print_percent(va_list ap, params_t *params)
 {
 	(void)ap;
 	(void)params;
-	return (_putchar('%'));
+	return _putchar('%');
 }
 
 /**
@@ -106,27 +115,31 @@ int print_percent(va_list ap, params_t *params)
  */
 int print_S(va_list ap, params_t *params)
 {
-	char *str = va_arg(ap, char *);
-	char *hex;
-	int sum = 0;
+	char *text = va_arg(ap, char *);
+	char *hexadecimal;
+	int count = 0;
 
-	if ((int)(!str))
-		return (_puts(NULL_STRING));
-	for (; *str; str++)
+	if ((int)(!text))
+		return _puts(NULL_STRING);
+
+	for (; *text; text++)
 	{
-		if ((*str > 0 && *str < 32) || *str >= 127)
+		if ((*text > 0 && *text < 32) || *text >= 127)
 		{
-			sum += _putchar('\\');
-			sum += _putchar('x');
-			hex = convert(*str, 16, 0, params);
-			if (!hex[1])
-				sum += _putchar('0');
-			sum += _puts(hex);
+			count += _putchar('\\');
+			count += _putchar('x');
+			hexadecimal = convert(*text, 16, 0, params);
+
+			if (!hexadecimal[1])
+				count += _putchar('0');
+
+			count += _puts(hexadecimal);
 		}
 		else
 		{
-			sum += _putchar(*str);
+			count += _putchar(*text);
 		}
 	}
-	return (sum);
+
+	return count;
 }
