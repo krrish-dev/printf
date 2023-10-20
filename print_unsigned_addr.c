86f41694966551e451bf5ee67a7351ea765499f6
krrish_dev <justbegintoday@gmail.com>
2023-10-20 04:14:11 +0300

    test

--- print_unsigned_addr.c
+++ print_unsigned_addr.c
@@ -55,7 +55,7 @@ int print_unsigned(va_list ap, params_t *params)
 		l = (unsigned short int)va_arg(ap, unsigned int);
 	else
 		l = (unsigned int)va_arg(ap, unsigned int);
-	params->unsign = 1;
+	params->is_unsign = 1;
 	return (print_number(convert(l, 10, CONVERT_UNSIGNED, params), params));
 }
 
